9e9c0fb2fee8c315720ed7ceb2afdbaadb1ca85d
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-18 11:13:15 +0530

    Duties component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [isloading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async() => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  if(isloading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    )
+  }
+
+  return(
+    <section className="jobs-center">
+      <JobInfo jobs={jobs}/>
+    </section>
+  )
 };
 export default App;
